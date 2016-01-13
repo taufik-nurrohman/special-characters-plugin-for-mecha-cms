537d9e81c88097af19629696e3c14796e12db20c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-01-13 09:16:43 +0700

    For Mecha 1.2.x

--- __special-characters/launch.php
+++ __special-characters/launch.php
@@ -1,15 +1,13 @@
 <?php
 
-$speak = Config::speak();
-
 Config::merge('DASHBOARD.languages.MTE', array(
-    'plugin_special_character' => Mecha::A($speak->plugin_special_character)
+    'plugin_special_character' => (array) $speak->plugin_special_character
 ));
 
 Weapon::add('shell_after', function() {
-    echo Asset::stylesheet('cabinet/plugins/' . File::B(__DIR__) . '/assets/shell/button.css');
+    echo Asset::stylesheet(__DIR__ . DS . 'assets' . DS . 'shell' . DS . 'button.css');
 }, 20);
 
 Weapon::add('SHIPMENT_REGION_BOTTOM', function() {
-    echo Asset::javascript('cabinet/plugins/' . File::B(__DIR__) . '/assets/sword/button.js');
+    echo Asset::javascript(__DIR__ . DS . 'assets' . DS . 'sword' . DS . 'button.js');
 }, 20);
